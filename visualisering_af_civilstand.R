44f1c69b6328462781e8ffd6e659be218bfede99
Anneaslai <162146301+Anneaslai@users.noreply.github.com>
2024-05-22 13:23:08 +0200

    Update visualisering_af_civilstand.R

--- visualisering_af_civilstand.R
+++ visualisering_af_civilstand.R
@@ -2,7 +2,7 @@
 
 #DATASÆT FRA 1801 
 
-library(tidyverse) #loader den pakke, som er nødvendig for at kunne bruge ggplot2
+library(tidyverse)
 
 civilstand_1801 <- read_csv ("/Users/anneaslai-sorensen/Documents/Historie AU/2. Semester/Digitale arkiver og metoder/Eksamen/Csv data filer/datasæt_20_til_31_fra_1801.txt")
 
@@ -31,8 +31,8 @@ civilstand_1801 %>%
   drop_na(alder) %>% 
   mutate(alder_str=paste0(alder, " år")) %>% 
   ggplot(aes(x=civilstand, fill=civilstand))+
-  geom_bar(stat = 'count', position="dodge")+ # stat='count   #'dodge' bevarer den vertikale del af geom_bar, mens den justerer den horizontale del
-  facet_wrap(~alder_str, ncol = 3)+ #fordeler det ud til flere diagrammer med tre på hver række
+  geom_bar(stat = 'count', position="dodge")+ 
+  facet_wrap(~alder_str, ncol = 3)+ 
   theme_minimal()+
   ggtitle("Civilstand fordelt på alder i 1801")
 
@@ -58,7 +58,7 @@ civilstand_2000_2020 %>%
   geom_col(stat = "identity", position="dodge")+
   facet_wrap(~Alder, ncol = 3)+
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))+ #vinkler x-aksens tekst i den vinkel man ønsker
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))+ 
   ggtitle("Civilstand fordelt på alder fra 2000 til 2020")
 
 
